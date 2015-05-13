81a46d13219a07c6c54d2c4711542da487416375
Jacy Wang <jacywj@gmail.com>
2015-05-12 19:03:30 -0700

    Add rollDice

--- Threelow/ViewController.m
+++ Threelow/ViewController.m
@@ -10,6 +10,14 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIButton *dice1;
+@property (strong, nonatomic) IBOutlet UIButton *dice2;
+@property (strong, nonatomic) IBOutlet UIButton *dice3;
+@property (strong, nonatomic) IBOutlet UIButton *dice4;
+@property (strong, nonatomic) IBOutlet UIButton *dice5;
+@property (strong, nonatomic) NSArray *diceImageArray;
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +25,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.diceImageArray = @[
+                        [UIImage imageNamed:@"dice_1.png"],
+                        [UIImage imageNamed:@"dice_2.png"],
+                        [UIImage imageNamed:@"dice_3.png"],
+                        [UIImage imageNamed:@"dice_4.png"],
+                        [UIImage imageNamed:@"dice_5.png"],
+                        [UIImage imageNamed:@"dice_6.png"],
+                        ];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)rollDiceButtonPressed:(UIButton *)sender {
+    int numberArray[5];
+    for (int i = 0; i < 5; i++) {
+        int randomNumber = arc4random_uniform(6);
+        numberArray[i] = randomNumber;
+    }
+    
+    self.dice1.imageView.image = self.diceImageArray[numberArray[0]];
+    self.dice2.imageView.image = self.diceImageArray[numberArray[1]];
+    self.dice3.imageView.image = self.diceImageArray[numberArray[2]];
+    self.dice4.imageView.image = self.diceImageArray[numberArray[3]];
+    self.dice5.imageView.image = self.diceImageArray[numberArray[4]];
+    
+}
+
+- (IBAction)resetDiceButtonPressed:(UIButton *)sender {
 }
 
 @end
